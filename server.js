7f625b2c37b63fc92717ba68c0af76db2433c34c
Axel <fonso499@gmail.com>
2026-04-08 11:56:47 -0600

    fix gitignore

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Database = require("better-sqlite3");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const session = require("express-session");
 
 const app = express();
